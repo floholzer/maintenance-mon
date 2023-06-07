aa7536fbeb978d2b4a36787946fa14c30af4b74e
floholzer <Florentin.Holzer@gmx.at>
2023-06-07 08:08:22 +0200

    inital commit

---
+++ src/main/java/at/technikum/maintenancemon/MaintenanceMonApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.maintenancemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceMonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaintenanceMonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/technikum/maintenancemon/MaintenanceMonApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.maintenancemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceMonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
